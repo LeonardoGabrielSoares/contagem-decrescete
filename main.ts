9b3758c6443c49a529166aebb0a1a189df617cec
LeonardoGabrielSoares <100873343+LeonardoGabrielSoares@users.noreply.github.com>
2022-03-11 22:11:37 +0000

    Ficheiros iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,34 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("10")
+    basic.pause(1000)
+    basic.showString("9")
+    basic.pause(1000)
+    basic.showString("8")
+    basic.pause(1000)
+    basic.showString("7")
+    basic.pause(1000)
+    basic.showString("6")
+    basic.pause(1000)
+    basic.showString("5")
+    basic.pause(1000)
+    basic.showString("4")
+    basic.pause(1000)
+    basic.showString("3")
+    basic.pause(1000)
+    basic.showString("2")
+    basic.pause(1000)
+    basic.showString("1")
+    basic.pause(1000)
+    basic.showString("0")
+    basic.pause(1000)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
+basic.forever(function () {
+	
+})
